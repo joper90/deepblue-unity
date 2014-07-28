b13c48af749b39a4c2f900a1a65ac8ee4ccdf63f
joe90 <joper90@gmail.com>
2014-07-28 21:36:04 +0100

    Watch rotation working

---
+++ deepblue/Assets/scripts/RotateAroundPoint.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAroundPoint : MonoBehaviour {
+
+	public float amountAppear = 1f;
+	public float amountDisappear = -1f;
+
+	private float startAngle = 270f;
+	private float uprightAngle = 340f;
+
+	private float currentAngle = 270f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		currentAngle = transform.eulerAngles.z;
+
+		if (Input.GetKey (KeyCode.Space))
+	    {
+			if (currentAngle < uprightAngle)
+			{
+				transform.Rotate (0,  0, amountAppear * Time.deltaTime);
+			}
+
+		}
+		else
+		{
+			if (currentAngle > startAngle)
+			{
+				transform.Rotate (0,  0, amountDisappear * Time.deltaTime);
+			}
+		}
+		
+	}
+}
